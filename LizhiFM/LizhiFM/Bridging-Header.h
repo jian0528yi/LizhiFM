4aafbbf28732b63694ae4f30c2d28bb2c24b5d1f
jeremy <jeremy@o2fun.com>
2016-11-24 22:07:17 +0800

    添加桥接文件。

---
+++ LizhiFM/LizhiFM/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  LizhiFM
+//
+//  Created by JLB on 2016/11/24.
+//  Copyright © 2016年 LB. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "Kingfisher/Kingfisher.h"
+
+#endif /* Bridging_Header_h */
